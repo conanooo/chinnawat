1d6b18a0815f2d2d0705415ffbc952f611c20332
LAB3201_00 <vvccx5555@gmail.com>
2018-01-13 15:30:55 +0700

    Create Alert Dialog

--- app/src/main/java/chinnawat/sbc/ac/th/sbcmoneychange/fragment/CalculateFragment.java
+++ app/src/main/java/chinnawat/sbc/ac/th/sbcmoneychange/fragment/CalculateFragment.java
@@ -1,14 +1,18 @@
 package chinnawat.sbc.ac.th.sbcmoneychange.fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.text.GetChars;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import chinnawat.sbc.ac.th.sbcmoneychange.MainActivity;
 import chinnawat.sbc.ac.th.sbcmoneychange.R;
@@ -42,11 +46,61 @@ public class CalculateFragment extends Fragment{
         toolbarController();
 
 //        Get Value From Argument
-        aDouble = getArguments().getDouble("Factor");
-        Log.d("13Jan","Factor==>"+aDouble);
+        getValueFromArgument();
 
+//        calculate controller
+        calculateController();
 
     }//Main Method
+
+    private void calculateController() {
+        Button button=getView().findViewById(R.id.btnCalculate);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Get Value From EdiText
+                EditText editText=getView().findViewById(R.id.edtmonay);
+                String moneyString =editText.getText().toString().trim();
+
+//                Check Space
+                if (moneyString.isEmpty()) {
+//                    Have space
+                    myAlert("Have Space", "Please Fill Money in Blank");
+
+                } else {
+//                    No space
+                }
+
+
+
+
+            }//onclick
+        });
+    }
+
+    private void myAlert(String titleString, String messageString) {
+
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.ic_action_alert);
+        builder.setTitle(titleString);
+        builder.setMessage(messageString);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+
+    }//myAlert
+
+    private void getValueFromArgument() {
+        aDouble = getArguments().getDouble("Factor");
+        Log.d("13Jan","Factor==>"+aDouble);
+    }
 
     private void toolbarController() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);

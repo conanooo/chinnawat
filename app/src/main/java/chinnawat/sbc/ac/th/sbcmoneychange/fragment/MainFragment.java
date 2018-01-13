cc42cf273adf4bc701848073afaf372592ba9d92
LAB3201_00 <vvccx5555@gmail.com>
2018-01-13 10:46:35 +0700

    Replace Fragment Success

--- app/src/main/java/chinnawat/sbc/ac/th/sbcmoneychange/fragment/MainFragment.java
+++ app/src/main/java/chinnawat/sbc/ac/th/sbcmoneychange/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import chinnawat.sbc.ac.th.sbcmoneychange.R;
 
@@ -14,6 +15,30 @@ import chinnawat.sbc.ac.th.sbcmoneychange.R;
  */
 
 public class MainFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Calculate Controller
+
+        Button button = getView().findViewById(R.id.btnCalculate);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                replace Frament
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new CalculateFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+
+
+    }//Main Method
 
     @Nullable
     @Override
